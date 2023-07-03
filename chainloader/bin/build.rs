f68e0191800a4474d04730a4498618b31c14ba19
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-07-03 09:33:26 +0200

    few minor adjustments; no effect on objdump

--- chainloader/bin/build.rs
+++ chainloader/bin/build.rs
@@ -1,5 +1,10 @@
 fn main() {
-    let linker_script = "bin/link.ld";
-    println!("cargo:rerun-if-changed={linker_script}");
-    println!("cargo:rustc-link-arg=-T{linker_script}");
+    let linker_script = "link.ld";
+    // Weird: This path must not include the "bin/":
+    let rerun_if_changed_path = linker_script;
+    //        but this path must include the "bin/":
+    let linker_arg_path = format!("bin/{linker_script}");
+
+    println!("cargo:rerun-if-changed={rerun_if_changed_path}");
+    println!("cargo:rustc-link-arg=-T{linker_arg_path}");
 }
